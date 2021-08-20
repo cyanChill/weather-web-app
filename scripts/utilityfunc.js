6b2f04c279f42bebf8c8392e045858f1e0cef13a
cyanchill <anthonyliang9@gmail.com>
2021-08-20 19:17:28 -0400

    replaced previous function to get YMD date format with function that returns object of date values

--- scripts/utilityfunc.js
+++ scripts/utilityfunc.js
@@ -1,59 +1,52 @@
 /* Utility Functions For Weather Web App */
 
+export function getNiceDate() {
+  const d = new Date();
+  let year = d.getFullYear();
+  let month = d.getMonth() < 9 ? `0${d.getMonth() + 1}` : `${d.getMonth() + 1}`;
+  let day = d.getDate() < 10 ? `0${d.getDate()}` : `${d.getDate()}`;
+  let hour = d.getHours();
+  let niceHour = hour < 9 ? hour + 1 : hour;
+  let minutes = d.getMinutes() + 1;
+  let niceMinutes = minutes < 10 ? `0${minutes}` : minutes;
 
-/* 
-    Get date in format year-month-day where year is 4-digits, month & day
-    are 2-digits
-*/
-export function getYearMonthDay() {
-    const d = new Date();
-    let [year, month, day] = [d.getFullYear(), d.getMonth(), d.getDate()];
-    month++;
-    if (month < 10) {
-        month = `0${month}`;
-    }
-    if (day < 10) {
-        day = `0${day}`;
-    }
-    return `${year}-${month}-${day}`;
+  return { year, month, day, hour, niceHour, minutes, niceMinutes };
 }
-
 
 /* 
     Create the string used for the "temp" class div containing the tempearture
     of the widget
 */
 export function correctTemp(temp) {
-    /* temp is in Fahrenheit*/
-    const unit = localStorage.getItem('unit');
-    if (unit == 'F') {
-        return `${temp}&deg;F`
-    }
-    temp = (temp - 32) * 5 / 9;
-    return `${Math.round(temp)}&deg;C`
+  /* temp is in Fahrenheit*/
+  const unit = localStorage.getItem("unit");
+  if (unit == "F") {
+    return `${temp}&deg;F`;
+  }
+  temp = ((temp - 32) * 5) / 9;
+  return `${Math.round(temp)}&deg;C`;
 }
-
 
 /* 
     Get all elements with "temp" class do conversions if the new temperature 
     unit is different than the old temperature unit 
 */
 export function updateTemperature() {
-    const currUnit = localStorage.getItem('unit');
-    const tempArray = document.querySelectorAll('.temp');
-    const prevUnit = tempArray[0].innerHTML.split('°')[1];
-    if (currUnit == prevUnit) {
-        return false;
+  const currUnit = localStorage.getItem("unit");
+  const tempArray = document.querySelectorAll(".temp");
+  const prevUnit = tempArray[0].innerHTML.split("°")[1];
+  if (currUnit == prevUnit) {
+    return false;
+  }
+  tempArray.forEach((entry) => {
+    let temp = entry.innerHTML.split("°")[0];
+    if (currUnit == "C") {
+      temp = ((temp - 32) * 5) / 9;
+      entry.innerHTML = `${Math.round(temp)}&deg;C`;
+    } else {
+      temp = (temp * 9) / 5 + 32;
+      entry.innerHTML = `${Math.round(temp)}&deg;F`;
     }
-    tempArray.forEach(entry => {
-        let temp = entry.innerHTML.split('°')[0];
-        if (currUnit == 'C') {
-            temp = (temp - 32) * 5 / 9;
-            entry.innerHTML = `${Math.round(temp)}&deg;C`;
-        } else {
-            temp = (temp * 9 / 5) + 32;
-            entry.innerHTML = `${Math.round(temp)}&deg;F`;
-        }
-    });
-    console.log(`Updated previous temperature in ${prevUnit} into ${currUnit}.`)
+  });
+  console.log(`Updated previous temperature in ${prevUnit} into ${currUnit}.`);
 }
