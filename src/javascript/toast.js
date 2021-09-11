1978170b1558cc0a83e45060b87d26ddb00f78c4
cyanchill <anthonyliang9@gmail.com>
2021-09-11 18:48:29 -0400

    add toast funcitonality

---
+++ src/javascript/toast.js
@@ -0,0 +1,45 @@
+import { closestTo } from "date-fns";
+
+const toastModule = (function () {
+  const toastLocation = document.getElementById("toast");
+
+  function displayToast(msg, type) {
+    const toast = document.createElement("div");
+    const toastType = document.createElement("span");
+    const toastMsg = document.createElement("span");
+    const closeToast = document.createElement("span");
+
+    toast.classList = `toast ${type === "error" ? "error" : type === "success" ? "sucess" : ""}`;
+    toastType.classList = "toast-type";
+    toastMsg.classList = "toast-msg";
+    closeToast.classList = "remove-toast";
+
+    toastType.textContent = type === "error" ? "Error:" : type === "success" ? "Success:" : "";
+    toastMsg.textContent = msg;
+    closeToast.textContent = "×";
+
+    toast.appendChild(toastType);
+    toast.appendChild(toastMsg);
+    toast.appendChild(closeToast);
+
+    const removeTime = setTimeout(() => {
+      removeToast(toast);
+    }, 10000);
+
+    closeToast.addEventListener("click", () => {
+      removeToast(toast, removeTime);
+    });
+
+    toastLocation.insertBefore(toast, toastLocation.firstChild);
+  }
+
+  function removeToast(toast, timeout) {
+    clearTimeout(timeout);
+    toast.removeEventListener("click", removeToast);
+    toast.remove();
+  }
+
+  return { displayToast };
+})();
+
+export default toastModule;
